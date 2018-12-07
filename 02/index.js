7fc1dd140d39afd9a20515c40327d291f1cdf049
momentiris <momentiris@gmail.com>
2018-12-07 17:01:07 +0100

    day

--- 02/index.js
+++ 02/index.js
@@ -1,20 +1,54 @@
 const fs = require('fs')
 
 const input = fs.readFileSync(__dirname + '/input.txt', 'utf-8')
-  .split('\n').map(x => x.split(''))
+  .split('\n')
+ 
+// part 1 
+const first = input
+  .map(x => x.split('')
+  .sort()
+  .join(''))
+  .reduce((acc, val) => {
+    const match = val.match(/(.)\1+/g)
+    const two = match.filter(x => x.length === 2).length 
+    const three = match.filter(x => x.length === 3).length 
 
-const instances = 
-  input.map(row => {
-    
-  const hits = {}
-  return row.reduce((obj, letter) => {
-    if (!obj[letter]) {
-      obj[letter] = 0;
-    }
-    obj[letter]++
-    return obj;
-  }, hits)
-})
+    return [
+      acc[0] = two ? acc[0] + 1 : acc[0],
+      acc[1] = three ? acc[1] + 1 : acc[1],
+    ]
+  }, [0, 0])
+  .reduce((a, b) => a * b)
+
+  console.log(first);
+
+
+// part 2
+const findPlease = (input) => {
+  let hit
+  splitInput.forEach((line, i) => {    
+    splitInput.slice(i).forEach((sl, si) => {
+      if(sl.filter((slsl, sisi) => slsl === line[sisi]).length === 25) {
+        hit = [line, sl]
+      }
+    })
+  })
+  console.log(hit);
+}
+
+const splitInput = input.map(x => x.split(''))
+findPlease(splitInput)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
