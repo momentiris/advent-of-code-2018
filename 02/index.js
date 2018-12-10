bf135c81e8490396a5fee30d616e794b34bb8372
momentiris <momentiris@gmail.com>
2018-12-10 22:29:58 +0100

    ok

--- 02/index.js
+++ 02/index.js
@@ -3,38 +3,64 @@ const fs = require('fs')
 const input = fs.readFileSync(__dirname + '/input.txt', 'utf-8')
   .split('\n').map(x => x.split(''))
 
-const instances = 
-  input.map(row => {
+const counter = data => 
+  data.map(row => 
+    row.reduce((obj, l) => {
+      if (!obj[l]) obj[l] = 0;
+      obj[l]++
+
+      return obj
+    }, {})
+  )
+let twos = 0
+let threes = 0
+
+const test = [
+  ...counter(input)
+]
+
+const hej = test.forEach(inst => {
+  const ok = {}
+  Object.inst.reduce((obj, x) => {
+    console.log(obj);
     
-  const hits = {}
-  return row.reduce((obj, letter) => {
-    if (!obj[letter]) {
-      obj[letter] = 0;
-    }
-    obj[letter]++
-    return obj;
-  }, hits)
+    return obj
+  }, ok)
+  
 })
 
 
 
 
+  // const firstCount = counter(input).map(row => {
+
+  //    Object.entries(row).filter(x => x[1] > 1).forEach(x => {
+  //      console.log(x);
+       
+  //    })
 
 
+            
+    
+        
 
 
+  //   // .forEach(y => {
+  //   //       y.forEach(z => {
+  //   //           console.log(z);
+  //   //       })
+  //   //       return sum
+  //   //     })
+  //   // return sum
 
+  // })
 
-
-
-
-
-
-
+  
 
   
 
 
-
-
-
+  
+    
+    
+   
